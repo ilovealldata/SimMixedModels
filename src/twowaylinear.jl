a7f5087991898ae09a87fe4dbb0b138eeaeef1ac
Yonghee Lee <ilovealldata@gmail.com>
2015-09-01 11:43:38 +0900

    new twoway function

--- src/twowaylinear.jl
+++ src/twowaylinear.jl
@@ -52,6 +52,38 @@ function makedata2waylinear(Timelength::Int64,Idnum::Int64, Groupnum::Int64,
 		              Sig10, Sig2, Esig, re, n, dat)
 end 
 
+function makedata2waylinear(Timepoints::Array{Float64,1},Idnum::Int64, Groupnum::Int64,
+		Beta::Array{Float64,1} ,Sig10::Array{Float64,1},Sig2::Float64,Esig::Float64, timegap::Float64 )
+	
+	nt = length(Timepoints)
+	n=nt*Idnum
+	grplength=convert(Int64,n/Groupnum)
+	ggrp = [1:Groupnum]
+	idd=[1:Idnum]
+	ttime=Timepoints
+	id=pool(repeat(idd,inner=[nt]))
+	time= repeat(ttime,outer=[Idnum])
+	grp=pool(repeat(ggrp,inner=[grplength]))
+	n=size(id,1)
+	X = hcat(repeat([1.0],outer=[n]),time)
+	sig1=reshape(Sig10,2,2)
+	r1=vec(rand(MvNormal([0.0,0.0], sig1),Idnum))
+	Z1=kron(speye(Idnum),sparse(hcat(ones(nt),Timepoints)))
+	if Sig2 > 0.0
+	  r2=rand(Normal(0.0,Sig2),Groupnum)
+	  Z2 = kron(speye(Groupnum),sparse(ones(grplength)))
+	  y = X*Beta+Z1*r1+Z2*r2+rand(Normal(0.0,Esig),n)
+	  re={r1,r2}
+    else
+      y = X*Beta+Z1*r1+rand(Normal(0.0,Esig),n)
+      re={r1}
+    end  	
+    
+	dat=DataFrame(time=time,id=id,grp=grp,y=y)
+	ReplinearOnegroup(nt,Idnum, Groupnum, Beta, 
+		              Sig10, Sig2, Esig, re, n, dat)
+end 
+
 # generate new y conditional on same random effects in md
 function newdata2waylinear(md::ReplinearOnegroup, timegap::Float64)
 	n=md.Timelength*md.Idnum
